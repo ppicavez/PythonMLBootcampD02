8f97fc478ae9957408c8126ae2c4a334c65b17dc
ppicavez <ppicavez@student.42lyon.fr>
2020-07-08 16:26:17 +0200

    ex05 correction

--- ex05/gradient.py
+++ ex05/gradient.py
@@ -20,3 +20,25 @@ def gradient(x, y, theta):
     X_prime = np.c_[np.ones((len(x), 1)), x]
     result = (X_prime.T @ (X_prime @ theta - y)) / len(x)
     return result
+
+
+if __name__ == "__main__":
+    x = np.array([
+    [ -6, -7, -9],
+    [ 13, -2, 14],
+    [ -7, 14, -1],
+    [ -8, -4, 6],
+    [ -5, -9, 6],
+    [ 1, -5, 11],
+    [ 9, -11, 8]])
+    y = np.array([2, 14, -13, 5, 12, 4, -19])
+    theta1 = np.array([0, 3, 0.5, -6])
+    # Example :
+    print(gradient(x, y, theta1))
+    # Output:
+    # array([ -37.35714286, 183.14285714, -393. ])
+    # Example :
+    theta2 = np.array([0,0,0,0])
+    print(gradient(x, y, theta2))
+    # Output:
+    # array([ 0.85714286, 23.28571429, -26.42857143])
